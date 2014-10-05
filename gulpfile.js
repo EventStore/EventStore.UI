527d5143e87e9484fc5af2af159bb4dc33b155ab
Jakub Gutkowski <j.gutkowski@gmail.com>
2014-10-06 00:09:30 +0200

    creating config settings for require.js optimization #19

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,56 @@ var paths = {
     tests: './test/**/*.js'
 };
 
+var rjsOpts = {
+    baseUrl: './src/js/',
+    name: 'bootstrap',
+    out: 'app.min.js',
+    shim: {
+        'angular': {
+            exports: 'angular'
+        },
+        'ngCookies': {
+            deps: ['angular'],
+            exports: 'ngCookies'            
+        },
+        'uiRouter': {
+            deps: ['angular']
+        },
+        'uiAce': {
+            deps: ['angular', 'ace']
+        },
+        'uiUtils': {
+            deps: ['angular']
+        },
+        'angularMocks': {
+            deps: ['angular'],
+            'exports': 'angular.mock'
+        },
+        'sprintf': {
+            exports: 'sprintf'
+        },
+        'domReady': {
+            exports: 'domReady'
+        }
+    },
+    priority: [
+        'angular'
+    ],
+    paths: {
+        'domReady': '../bower_components/requirejs-domready/domReady',
+        'angular': '../bower_components/angular/angular',
+        'ngCookies': '../bower_components/angular-cookies/angular-cookies',
+        'angularMocks': '../bower_components/angular-mocks/angular-mocks',
+        'uiRouter': '../bower_components/angular-ui-router/release/angular-ui-router',
+        'uiUtils': '../bower_components/angular-ui-utils/ui-utils',
+        'jQuery': '../bower_components/jQuery/dist/jquery',
+        'sprintf': '../bower_components/sprintf/src/sprintf',
+        'uiAce': '../bower_components/angular-ui-ace/ui-ace',
+        'ace': '../bower_components/ace-builds/src-min-noconflict/ace',
+        'es-ui': './app'
+    }
+};
+
 gulp.task('bower', function() {
     bower();
 });
