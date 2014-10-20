d55b4de5fa2cb82ab38ed91a01b02c2281400379
Jakub Gutkowski <j.gutkowski@gmail.com>
2014-10-20 11:44:05 +0200

    updating bower components and fixing bower.json so it's reference proper version of libs. this fix #34

--- gulpfile.js
+++ gulpfile.js
@@ -63,10 +63,6 @@ var rjsOpts = {
         'uiUtils': {
             deps: ['angular']
         },
-        'angularMocks': {
-            deps: ['angular'],
-            'exports': 'angular.mock'
-        },
         'sprintf': {
             exports: 'sprintf'
         }
@@ -78,15 +74,12 @@ var rjsOpts = {
         'domReady': '../bower_components/requirejs-domready/domReady',
         'angular': '../bower_components/angular/angular',
         'ngCookies': '../bower_components/angular-cookies/angular-cookies',
-        'angularMocks': '../bower_components/angular-mocks/angular-mocks',
         'uiRouter': '../bower_components/angular-ui-router/release/angular-ui-router',
         'uiUtils': '../bower_components/angular-ui-utils/ui-utils',
         'jQuery': '../bower_components/jQuery/dist/jquery',
         'sprintf': '../bower_components/sprintf/src/sprintf',
         'uiAce': '../bower_components/angular-ui-ace/ui-ace',
         'ace': '../bower_components/ace-builds/src-min-noconflict/ace',
-        //'ace-js': '../bower_components/ace-builds/src-min-noconflict/mode-javascript',
-        //'ace-monokai': '../bower_components/ace-builds/src-min-noconflict/theme-monokai',
         'es-ui': './app'
     }
 };
--- src/js/services/SprintfService.js
+++ src/js/services/SprintfService.js
@@ -9,7 +9,13 @@ define(['./_module', 'sprintf'], function (app, sprintf) {
 				format: function () {
 					var args = [].slice.call(arguments);
 
-					return sprintf.apply(null, args);
+					// this has changed, sprintf is an object now
+					// with two methods:
+					// - sprintf
+					// - vsprintf - same as sprintf but accepting array of objects
+					// therefore when we do require sprintf we are getting
+					// these two methods.
+					return sprintf.sprintf.apply(null, args);
 				}
 			};
 		}
