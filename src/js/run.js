1a56a676fe839f0536c5adb1d90a1da8b3b4d508
pgermishuys <pieter@pieterg.com>
2014-12-29 19:55:47 +0200

    Set configuration for Toastr, to ensure that the user confirms the notification and that it doesn't dissapear automatically

--- src/js/run.js
+++ src/js/run.js
@@ -1,7 +1,11 @@
 define(['es-ui'], function (app) {
 
 	'use strict';
-
+    app.config(function(toastrConfig){
+        angular.extend(toastrConfig, {
+            timeOut: 0
+        })
+    })
 	return app.run([
         '$rootScope', '$state', '$stateParams', 'AuthService', 'InfoService',
         function ($rootScope, $state, $stateParams, authService, infoService) {
