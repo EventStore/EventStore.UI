e1b8894cdb12776734f020182a4a2b84b5eafc63
pgermishuys <pieter@pieterg.com>
2014-12-30 17:27:10 +0200

    Add the close button, just so that the user is given a visual queue to dismiss the notification

--- src/js/run.js
+++ src/js/run.js
@@ -3,6 +3,7 @@ define(['es-ui'], function (app) {
 	'use strict';
     app.config(function(toastrConfig){
         angular.extend(toastrConfig, {
+            closeButton: true,
             timeOut: 0,
             positionClass: 'toast-top-full-width',
             preventDuplicates: true
