379baad4de11d384f5e8a635a343a39b29fea061
pgermishuys <pieter@pieterg.com>
2015-01-12 18:33:06 +0200

    Add a tip explaining to the user that tapping on the notification will also dismiss it

--- src/js/services/MessageService.js
+++ src/js/services/MessageService.js
@@ -7,24 +7,28 @@ define(['./_module'], function (app) {
 			function getToastOptions(requiresUserAck, level){
 				return {
 					closeButton: requiresUserAck ? true : level == 'warning' ? true : false,
-					positionClass: requiresUserAck ? "toast-container-modal toast-bottom-full-width" : "toast-bottom-full-width",
+					positionClass: requiresUserAck ? 'toast-container-modal toast-bottom-full-width' : 'toast-bottom-full-width',
 					timeOut: requiresUserAck ? 0 : 
 							  level == 'warning' ? 5000 : 
 							  level == 'info' ? 2000 : 0
 				}
 			}
+			function getToastTitle(title, level){
+				title = title || level;
+				return title + ' (tap notification to dismiss)';
+			}
 			return {
 				success: function(text, title){
-					toastr.success(text, title  || 'Success', getToastOptions(false, 'success'));
+					toastr.success(text, getToastTitle(title, 'Success'), getToastOptions(false, 'success'));
 				},
 				info: function (text, title) {
-					toastr.info(text, title  || 'Info', getToastOptions(false, 'info'));
+					toastr.info(text, getToastTitle(title, 'Info'), getToastOptions(false, 'info'));
 				},
 				warn: function (text, title) {
-					toastr.warning(text, title  || 'Warning', getToastOptions(false, 'warning'));
+					toastr.warning(text, getToastTitle(title, 'Warning'), getToastOptions(false, 'warning'));
 				},
 				failure: function (text, title) {
-					toastr.error(text, title || 'Failure', getToastOptions(true, 'failure'));
+					toastr.error(text, getToastTitle(title, 'Failure'), getToastOptions(true, 'failure'));
 				},
 				confirm: function (text) {
 					return confirm(text);
