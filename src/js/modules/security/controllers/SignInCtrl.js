ef9f6ce80595c63f17286b062a8b462c0d105f37
pgermishuys <pieter@pieterg.com>
2015-01-13 03:46:21 +0200

    there might be an instance where the ui is already open and the node's run projection mode changes. On signing in, let's reevaluate the projections mode

--- src/js/modules/security/controllers/SignInCtrl.js
+++ src/js/modules/security/controllers/SignInCtrl.js
@@ -26,6 +26,7 @@ define(['./_module'], function (app) {
 				.success(function (info) {
 					$rootScope.esVersion = info.esVersion || '0.0.0.0';
                     $rootScope.esVersion = $rootScope.esVersion  == '0.0.0.0' ? 'development build' : $rootScope.esVersion;
+                    $rootScope.projectionsAllowed = info.projectionsMode != 'None';
 					authService.setCredentials($scope.log.username, $scope.log.password, $scope.log.server);
 					redirectToPreviousState();
 				})
