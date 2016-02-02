835e98a1a563484254da935a4eba79566ccdd032
HayleyC <hayleyjeancampbell@gmail.com>
2016-02-02 09:46:18 +0200

    Shows replica stats on the cluster status page.
    Sets up a poll that fetches stats for replica connections from the master node.
    These stats are then displayed in the UI, as well as how
    far behind master a node is and how long it will take to catch up.

--- src/js/modules/clusterstatus/controllers/ClusterStatusListCtrl.js
+++ src/js/modules/clusterstatus/controllers/ClusterStatusListCtrl.js
@@ -2,8 +2,11 @@ define(['./_module'], function (app) {
 
     'use strict';
 
-    return app.controller('ClusterStatusListCtrl', ['$scope', 'poller', 'ClusterStatusService', 'InfoService',
-		function ClusterStatusListCtrl($scope, poller, clusterStatusService, infoService) {
+    return app.controller('ClusterStatusListCtrl', ['$scope', 'poller', 'ClusterStatusService', 'InfoService', 'constants',
+		function ClusterStatusListCtrl($scope, poller, clusterStatusService, infoService, constants) {
+            $scope.replicas = [];
+            var masterNodeUrl = '';
+
 			infoService.getOptions().then(
 			function(response){
 				var options = response.data;
@@ -30,9 +33,101 @@ define(['./_module'], function (app) {
 			        } else {
 			            $scope.errorMessage = '';
 			            $scope.nodes = response.members;
+                        var master = getMasterNode();
+                        if(master) {
+                            var masterUrl = master.externalHttpIp + ':' + master.externalHttpPort;
+                            if(!replicaStatsQuery || masterUrl !== masterNodeUrl) {
+                                masterNodeUrl = masterUrl;
+                                setupReplicaStatsPoller();
+                            }
+                        }
 			        }
 			    });
 			}
+
+            var replicaStatsQuery;
+            function setupReplicaStatsPoller() {
+                replicaStatsQuery = poller.create({
+                    interval: constants.clusterStatus.replicaPollInterval,
+                    action: clusterStatusService.replicaStats,
+                    params: [masterNodeUrl]
+                });
+                replicaStatsQuery.start();
+                replicaStatsQuery.promise.then(null, null, function(response) {
+                    var master = getMasterNode();
+                    for(var i = 0; i < response.length; i++)
+                    {
+                        var replica = response[i];
+                        var prevStats = getPrevStatsForReplica(replica.connectionId, $scope.replicas);
+                        var node = getNodeForReplica(replica.subscriptionEndpoint, $scope.nodes);
+                        calculateReplicaStats(replica, master, node, prevStats);
+                    }
+                    $scope.replicas = response;
+                });
+            }
+
+            function calculateReplicaStats(replica, master, node, prevStats) {
+                replica.isCatchingUp = node.state === 'CatchingUp';
+                replica.bytesToCatchUp = master.writerCheckpoint - node.writerCheckpoint;
+
+                replica.catchupStartTime = new Date().getTime();
+                replica.catchupStartBytesSent = replica.totalBytesSent;
+
+                if(prevStats) {
+                    if(prevStats.isCatchingUp) {
+                        replica.catchupStartTime = prevStats.catchupStartTime;
+                        replica.catchupStartBytesSent = prevStats.catchupStartBytesSent;
+                    }
+                    var seconds = (new Date().getTime() - replica.catchupStartTime) / constants.clusterStatus.replicaPollInterval;
+                    var bytes = replica.totalBytesSent - replica.catchupStartBytesSent;
+                    replica.approxSpeed = bytes / seconds;
+                    var estimatedTime = Math.round(replica.bytesToCatchUp / replica.approxSpeed);
+                    if(estimatedTime < 1) {
+                        estimatedTime = 1;
+                    }
+                    replica.estimatedTime = formatEstimatedTime(estimatedTime);
+                }
+            }
+
+            function formatEstimatedTime(seconds)
+            {
+                function padNumber(num) {
+                    if(num < 10) {
+                        return '0' + num;
+                    }
+                    return num;
+                }
+                var hours = Math.floor((seconds % 86400) / 3600);
+                var minutes = Math.floor(((seconds % 86400) % 3600) / 60);
+                var secs = ((seconds % 86400) % 3600) % 60;
+                return padNumber(hours) + ':' + padNumber(minutes) + ':' + padNumber(secs);
+            }
+
+            function getPrevStatsForReplica(connectionId, prevReplicas) {
+                for(var i = 0; i < prevReplicas.length; i++) {
+                    if(prevReplicas[i].connectionId === connectionId) {
+                        return prevReplicas[i];
+                    }
+                }
+            }
+
+            function getNodeForReplica(endpoint, nodes) {
+                for(var i = 0; i < nodes.length; i++) {
+                    var internalTcpIp = nodes[i].internalTcpIp + ':' + nodes[i].internalTcpPort;
+                    if(internalTcpIp === endpoint) {
+                        return nodes[i];
+                    }
+                }
+            }
+
+            function getMasterNode() {
+                for(var i = 0; i < $scope.nodes.length; i++) {
+                    if($scope.nodes[i].state === 'Master') {
+                        return $scope.nodes[i];
+                    }
+                }
+            }
+
 		    $scope.$on('$destroy', function () {
 				poller.clear();
 			});
--- src/js/modules/clusterstatus/services/ClusterStatusService.js
+++ src/js/modules/clusterstatus/services/ClusterStatusService.js
@@ -5,13 +5,22 @@ define(['./_module'], function (app) {
 	return app.provider('ClusterStatusService', function () {
 
 		this.$get = [
-			'$http', 'urls', 'UrlBuilder',
-			function ($http, urls, urlBuilder) {
+			'$http', 'urls', 'UrlBuilder', '$q',
+			function ($http, urls, urlBuilder, $q) {
 
 				return {
 					gossip: function () {
 						var url = urlBuilder.build(urls.gossip);
 
+						return $http.get(url);
+					},
+					replicaStats: function(masterUrl) {
+						if(!masterUrl) {
+							var deferred = $q.defer();
+							deferred.resolve({});
+							return deferred.promise;
+						}
+						var url = 'http://' + masterUrl + urls.replicationStats;
 						return $http.get(url);
 					}
 				};
