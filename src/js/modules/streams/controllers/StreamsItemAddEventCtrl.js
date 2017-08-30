290e32f9d290a967a97f3c474683728d0c977c55
Michael Gilliland <mjg.py3@gmail.com>
2017-08-30 06:59:46 -0400

    Basic add works.
    Still need to reset state.

--- src/js/modules/streams/controllers/StreamsItemAddEventCtrl.js
+++ src/js/modules/streams/controllers/StreamsItemAddEventCtrl.js
@@ -3,10 +3,13 @@ define(['./_module'], function (app) {
 
     return app.controller('StreamsItemAddEventCtrl', [
 		'$scope', '$state', '$stateParams', 'StreamsService', 'MessageService',
-		function ($scope, $state, $stateParams, streamsService, msg) {
+		function ($scope, $state, $stateParams, streams, msg) {
 			$scope.streamId = $stateParams.streamId;
 
                         $scope.eventId = generateArbitraryEventId();
+
+                        $scope.eventData = '{\n}';
+                        $scope.eventMetadata = '{\n}';
 
 			$scope.aceConfig = {
 				mode: 'json',
@@ -14,6 +17,19 @@ define(['./_module'], function (app) {
 				showGutter: true,
 				theme: 'monokai'
 			};
+
+                        $scope.addEvent = function () {
+                          streams
+                            .addEvent(
+                                $scope.streamId,
+                                {
+                                  eventId: $scope.eventId,
+                                  eventType: $scope.eventType,
+                                  data: JSON.parse($scope.eventData),
+                                  metadata: JSON.parse($scope.eventMetadata)
+                                }
+                            );
+                        };
 		}
 	]);
 
