6e8378ab340dd21286a97e954f7eaa758fa8ee0d
Kunal Dhingra <kdhingra55@gmail.com>
2022-04-26 18:14:58 -0400

    fix: update message for when stream or event does not exist

--- src/js/modules/projections/services/AtomEventsReader.js
+++ src/js/modules/projections/services/AtomEventsReader.js
@@ -40,10 +40,6 @@ define(['./_module'], function (app) {
 			}
 
 			function checkIfPoll (previous, headOfStream, onContinuePollCallback, doNotPollCallback) {
-
-				if (typeof result.previous.uri === "undefined") {
-					return;
-				}
 				if(!headOfStream){
 					doNotPollCallback();
 					return;
