45906364ef9bb101c55367567cd124c0396dbd60
Hayley Campbell <hayleyjeancampbell@gmail.com>
2025-01-23 12:24:42 +0100

    Fix whitespace

--- src/js/modules/competing/controllers/SubscriptionsListCtrl.js
+++ src/js/modules/competing/controllers/SubscriptionsListCtrl.js
@@ -4,37 +4,37 @@ define(['./_module'], function (app) {
 
     return app.controller('SubscriptionsListCtrl', [
         '$scope', 'CompetingService', 'SubscriptionsMapper', 'poller', 'MessageService', 'urls', 'UrlBuilder',
-		function ($scope, competingService, subscriptionsMapper, pollerProvider, msg, urls, urlBuilder) {
+        function ($scope, competingService, subscriptionsMapper, pollerProvider, msg, urls, urlBuilder) {
 
-			var subscriptionsPoll  = pollerProvider.create({
-				interval: 1000,
-				action: competingService.subscriptions,
-				params: []
-			});
+            var subscriptionsPoll = pollerProvider.create({
+                interval: 1000,
+                action: competingService.subscriptions,
+                params: []
+            });
 
-			$scope.replayParkedMessages = function(streamId, groupName){
-				competingService.replayParked(streamId, groupName).then(function () {
-					msg.success('Replaying Parked Messages');
-				}, function (error) {
-					msg.failure('Failed to initiate replaying of parked messages: ' + error.message);
-				});
-			};
+            $scope.replayParkedMessages = function (streamId, groupName) {
+                competingService.replayParked(streamId, groupName).then(function () {
+                    msg.success('Replaying Parked Messages');
+                }, function (error) {
+                    msg.failure('Failed to initiate replaying of parked messages: ' + error.message);
+                });
+            };
 
-			$scope.subscriptions = {};
+            $scope.subscriptions = {};
 
-			subscriptionsPoll.start();
-			subscriptionsPoll.promise.then(null, null, function (data) { 
-				$scope.subscriptions = subscriptionsMapper.map(data, $scope.subscriptions);
-			});
-			subscriptionsPoll.promise.catch(function (error) {
-				msg.failure('Failed to retrieve list of subscriptions: ' + error.message);
-				$scope.subscriptions = null;
-				subscriptionsPoll.stop(); 
-			});
-			
-			$scope.$on('$destroy', function () {
-				pollerProvider.clear();
-			});
-		}
-	]);
+            subscriptionsPoll.start();
+            subscriptionsPoll.promise.then(null, null, function (data) {
+                $scope.subscriptions = subscriptionsMapper.map(data, $scope.subscriptions);
+            });
+            subscriptionsPoll.promise.catch(function (error) {
+                msg.failure('Failed to retrieve list of subscriptions: ' + error.message);
+                $scope.subscriptions = null;
+                subscriptionsPoll.stop();
+            });
+
+            $scope.$on('$destroy', function () {
+                pollerProvider.clear();
+            });
+        }
+    ]);
 });
--- src/js/modules/competing/services/CompetingService.js
+++ src/js/modules/competing/services/CompetingService.js
@@ -1,72 +1,72 @@
 define(['./_module'], function (app) {
 
-	'use strict';
+    'use strict';
 
-	return app.provider('CompetingService', function () {
+    return app.provider('CompetingService', function () {
 
-		this.$get = [
-			'$http', 'urls', 'UrlBuilder',
-			function ($http, urls, urlBuilder) {
+        this.$get = [
+            '$http', 'urls', 'UrlBuilder',
+            function ($http, urls, urlBuilder) {
 
-				return {
-					subscriptions: function () {
-						var url = urlBuilder.build(urls.competing.subscriptions);
-						return $http.get(url);
-					},
-					subscriptionDetail: function(subscriptionId, groupName){
-						var url = urlBuilder.build(urls.competing.subscriptionDetails, subscriptionId, groupName);
-						return $http.get(url);	
-					},
-					create: function (stream, subscription, config) {
-						var url = urlBuilder.build(urls.competing.create, stream, subscription);
-						return $http.put(url, config, {
-							headers: {
-                                'Content-Type':'application/json'
+                return {
+                    subscriptions: function () {
+                        var url = urlBuilder.build(urls.competing.subscriptions);
+                        return $http.get(url);
+                    },
+                    subscriptionDetail: function (subscriptionId, groupName) {
+                        var url = urlBuilder.build(urls.competing.subscriptionDetails, subscriptionId, groupName);
+                        return $http.get(url);
+                    },
+                    create: function (stream, subscription, config) {
+                        var url = urlBuilder.build(urls.competing.create, stream, subscription);
+                        return $http.put(url, config, {
+                            headers: {
+                                'Content-Type': 'application/json'
                             }
-						});
-					},
-					update: function (stream, subscription, config) {
-						var url = urlBuilder.build(urls.competing.update, stream, subscription);
-						return $http.post(url, config, {
-							headers: {
-                                'Content-Type':'application/json'
+                        });
+                    },
+                    update: function (stream, subscription, config) {
+                        var url = urlBuilder.build(urls.competing.update, stream, subscription);
+                        return $http.post(url, config, {
+                            headers: {
+                                'Content-Type': 'application/json'
                             }
-						});
-					},
-					delete: function (stream, subscription) {
-						var url = urlBuilder.build(urls.competing.delete, stream, subscription);
-						return $http.delete(url, {
-							headers: {
-                                'Content-Type':'application/json'
+                        });
+                    },
+                    delete: function (stream, subscription) {
+                        var url = urlBuilder.build(urls.competing.delete, stream, subscription);
+                        return $http.delete(url, {
+                            headers: {
+                                'Content-Type': 'application/json'
                             }
-						});
-					},
-					replayParked: function (stream, subscription) {
-						var url = urlBuilder.build(urls.competing.replayParked, stream, subscription);
-						return $http.post(url, {}, {
-							headers: {
-                                'Content-Type':'application/json'
+                        });
+                    },
+                    replayParked: function (stream, subscription) {
+                        var url = urlBuilder.build(urls.competing.replayParked, stream, subscription);
+                        return $http.post(url, {}, {
+                            headers: {
+                                'Content-Type': 'application/json'
                             }
-						});
-					},
-					viewParkedMessagesBackward: function(streamId, groupName, fromEvent, count){
-						var url = urlBuilder.build(urls.competing.viewParkedMessagesBackward, streamId, groupName,fromEvent, count);
-						return $http.get(url, {
-							headers : {
-								'Accept':'application/vnd.eventstore.competingatom+json'
-							}
-						});	
-					},
-					viewParkedMessagesForward: function(streamId, groupName, fromEvent, count){
-						var url = urlBuilder.build(urls.competing.viewParkedMessagesForward, streamId, groupName,fromEvent, count);
-						return $http.get(url, {
-							headers : {
-								'Accept':'application/vnd.eventstore.competingatom+json'
-							}
-						});	
-					}
-				};
-			}
-		];
+                        });
+                    },
+                    viewParkedMessagesBackward: function (streamId, groupName, fromEvent, count) {
+                        var url = urlBuilder.build(urls.competing.viewParkedMessagesBackward, streamId, groupName, fromEvent, count);
+                        return $http.get(url, {
+                            headers: {
+                                'Accept': 'application/vnd.eventstore.competingatom+json'
+                            }
+                        });
+                    },
+                    viewParkedMessagesForward: function (streamId, groupName, fromEvent, count) {
+                        var url = urlBuilder.build(urls.competing.viewParkedMessagesForward, streamId, groupName, fromEvent, count);
+                        return $http.get(url, {
+                            headers: {
+                                'Accept': 'application/vnd.eventstore.competingatom+json'
+                            }
+                        });
+                    }
+                };
+            }
+        ];
     });
 });
